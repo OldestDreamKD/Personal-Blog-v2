01fbdf233bfaf3e9428a22493523299a771f6988
Karthik <karthik97vk@gmail.com>
2024-07-28 14:39:30 +0530

    Build complete with local server

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const blogEntry = require(__dirname + "/entries.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyparser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+mongoose.connect("mongodb://localhost:27017/entriesDB");
 
-let EntryPreview = blogEntry.entriesPreview();
-let EntryFull = blogEntry.entriesFull();
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+const entryschema = new mongoose.Schema({
+  title: String,
+  entry: String,
 });
 
-app.get("/", function (req, res) {
-  EntryPreview = blogEntry.entriesPreview();
-  for (var i = 0; i < EntryPreview.length; i++) {
-    EntryPreview[i].entry = EntryPreview[i].entry.substring(0, 100);
-  }
+const Entry = mongoose.model("Entry", entryschema);
 
-  res.render("homepage", {
-    newEntry: EntryPreview,
+app.get("/", function (req, res) {
+  const entry = new Entry({
+    title: "Day1",
+    entry: "This is my first entry",
+  });
+
+  Entry.find().then(function (Entry) {
+    res.render("homepage", {
+      Entry: Entry,
+    });
   });
 });
 
 app.post("/entry", function (req, res) {
-  EntryFull = blogEntry.entriesFull();
-  let id = req.body.entryI - 1;
-
-  res.render("entries", {
-    newEntryF: EntryFull,
-    id: id,
+  const entryID = req.body.entryID;
+  Entry.find({ _id: entryID }).then(function (Entry) {
+    res.render("entries", {
+      Entry: Entry,
+    });
   });
 });
 
@@ -40,16 +42,19 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  let Entrysize = EntryFull.length - 1;
-  let id = Number.parseInt(EntryFull[Entrysize].id) + 1;
-  let newEntry = {
-    id: id.toString(),
+  const entry = new Entry({
     title: req.body.entryTitle,
     entry: req.body.entryPost,
-  };
-
-  blogEntry.pushEntry(newEntry);
+  });
+  entry.save();
   res.redirect("/");
+});
+
+app.get("/delete/:entryID", function (req, res) {
+  const entryID = req.params.entryID;
+  Entry.deleteOne({ _id: entryID }).then(function (response) {
+    res.redirect("/");
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -58,4 +63,8 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function (req, res) {
   res.render("contact");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
